81cb585a85f416372a0e3dd0656d3d6250775ee2
Edmund Widl <edmund.e.widl@ieee.org>
2022-02-02 11:46:03 +0100

    update critical dependencies (log4j, etc.)

--- examples/setup.cmd
+++ examples/setup.cmd
@@ -6,7 +6,7 @@ REM Connection string for configuration server.
 SET LLCONFIG=http://localhost:10101/get?id=
 
 REM Version of plotter package.
-SET VERSION=0.0.3
+SET VERSION=0.0.4
 
 REM Root directory of plotter package (only change this if you really know what you are doing).
 SET PLOTTER_ROOT_DIR=%~DP0..
@@ -18,16 +18,16 @@ REM Directory containing Java class files for testing.
 SET PLOTTER_TEST_CLASS_DIR=%PLOTTER_ROOT_DIR%\target\test-classes
 
 REM Path to Java JAR file of data point bridge.
-SET DPB_JAR_FILE=%PLOTTER_ROOT_DIR%\target\dependency\dpbridge-0.0.1-jar-with-dependencies.jar
+SET DPB_JAR_FILE=%PLOTTER_ROOT_DIR%\target\dependency\dpbridge-0.0.2-jar-with-dependencies.jar
 
 REM Path to Java JAR file of standalone sync host.
-SET SYNC_JAR_FILE=%PLOTTER_ROOT_DIR%\target\dependency\sync-0.0.1-jar-with-dependencies.jar
+SET SYNC_JAR_FILE=%PLOTTER_ROOT_DIR%\target\dependency\sync-0.0.2-jar-with-dependencies.jar
 
 REM Path to Java JAR file of config server.
-SET CONFIG_JAR_FILE=%PLOTTER_ROOT_DIR%\target\dependency\config-0.1.0-jar-with-dependencies.jar
+SET CONFIG_JAR_FILE=%PLOTTER_ROOT_DIR%\target\dependency\config-0.1.1-jar-with-dependencies.jar
 
 REM Check if environment variable JAVA_HOME has been defined.
 IF NOT DEFINED JAVA_HOME (
-    ECHO WARNING: environment variable JAVA_HOME not has been defined!
+    ECHO WARNING: environment variable JAVA_HOME has not been defined!
     PAUSE
 )
